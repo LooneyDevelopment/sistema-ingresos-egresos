b04b65c7664df2a52b2c497438460ff8cd381d70
Wilmar Zapata <wavilla@stefanini.com>
2022-08-24 00:39:09 -0500

    Exportar proyecto maven creado con initilzr

---
+++ src/test/java/com/looneyDevelopment/sistemaingresosegresos/SistemaIngresosEgresosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looneyDevelopment.sistemaingresosegresos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaIngresosEgresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
