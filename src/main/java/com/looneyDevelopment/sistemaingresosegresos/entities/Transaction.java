01e179078485fcb5cad713ec955a6397ee8b6399
Wilmar Zapata <wavilla@stefanini.com>
2022-08-24 23:09:05 -0500

    Creando el paquete entities y la clase Transaction.java

---
+++ src/main/java/com/looneyDevelopment/sistemaingresosegresos/entities/Transaction.java
@@ -0,0 +1,45 @@
+package com.looneyDevelopment.sistemaingresosegresos.entities;
+
+
+public class Transaction {
+
+    private Long id;
+
+    private Float amount;
+
+    private String concept;
+
+    private Employee employee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
