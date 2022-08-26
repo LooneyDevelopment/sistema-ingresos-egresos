332727038aafd8281b55572b8ff867e690bdfc39
deivydumb <85261298+deivydumb@users.noreply.github.com>
2022-08-26 17:45:43 -0500

    Creacion de clase Enterprise

---
+++ src/main/java/com/looneyDevelopment/sistemaingresosegresos/entities/Enterprise.java
@@ -0,0 +1,78 @@
+package com.looneyDevelopment.sistemaingresosegresos.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "enterprise")
+public class Enterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String document;
+    private String phone;
+    private String address;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
