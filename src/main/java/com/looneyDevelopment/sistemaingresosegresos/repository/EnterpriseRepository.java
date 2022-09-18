5438387eff4bf7cb8a899509baa16f0fcee546d7
Saulo Andrés <saulocarmonatoro@gmail.com>
2022-09-18 13:06:24 -0500

    Agregando los servicios y repositorios

---
+++ src/main/java/com/looneyDevelopment/sistemaingresosegresos/repository/EnterpriseRepository.java
@@ -0,0 +1,9 @@
+package com.looneyDevelopment.sistemaingresosegresos.repository;
+
+import com.looneyDevelopment.sistemaingresosegresos.entities.Enterprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnterpriseRepository extends JpaRepository<Enterprise, String> {
+}
---
+++ src/main/java/com/looneyDevelopment/sistemaingresosegresos/service/EnterpriseService.java
@@ -0,0 +1,39 @@
+package com.looneyDevelopment.sistemaingresosegresos.service;
+
+import com.looneyDevelopment.sistemaingresosegresos.entities.Enterprise;
+import com.looneyDevelopment.sistemaingresosegresos.repository.EnterpriseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EnterpriseService {
+
+    @Autowired
+    private EnterpriseRepository enterpriseRepository;
+
+
+    public Enterprise saveEnterprise(Enterprise enterprise_param){ //Crea una fila en la database
+        return enterpriseRepository.save(enterprise_param);
+    }
+
+    public List<Enterprise> getEnterprises(){ //Halla todas las empresas en la database
+        return enterpriseRepository.findAll();
+    }
+
+    public Enterprise getEnterprise(String id) throws Exception { //Busca una empresa por su id
+        Optional<Enterprise> enterpriseOptional = enterpriseRepository.findById(id);
+        if (enterpriseOptional.isPresent()){
+            return enterpriseOptional.get();
+        }
+        else{
+            throw new Exception("La empresa no existe en al base de datos");
+        }
+    }
+    public Enterprise putEnterprise(Enterprise enterprise_param) {  //Actualiza datos
+        return enterpriseRepository.save(enterprise_param);
+    }
+}
+
